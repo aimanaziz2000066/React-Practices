e2b844a82a7b4eada8feda7eab14f2bab50abaab
aimanaziz <aimanaziz2000066@gmail.com>
2026-08-11 11:53:22 +0500

    Add styled card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,67 @@
+//Styled Component
+
+// import styled from "styled-components";
+
+// const Heading = styled.h1`
+//   color: purple;
+//   text-align: center;
+// `;
+// function App(){
+//   return(
+//     <>
+//     <Heading>Hello Styled Component!</Heading>
+//     </>
+//   )
+// }
+// export default App;
+
 import styled from "styled-components";
 
-const Heading = styled.h1`
-  color: purple;
+const Card = styled.div`
+  width: 300px;
+  padding: 25px;
+  margin: 50px auto;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
   text-align: center;
 `;
 
-function App(){
-  return(
-    <>
-    <Heading>Hello Styled Component!</Heading>
-    </>
-  )
+const Title = styled.h2`
+  color: purple;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.p`
+  color: gray;
+  font-size: 16px;
+`;
+
+const Button = styled.button`
+  background-color: purple;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: darkviolet;
+  }
+`;
+
+function App() {
+  return (
+    <Card>
+      <Title>Styled Card</Title>
+
+      <Text>
+        This card is created using styled-components.
+      </Text>
+
+      <Button>Learn More</Button>
+    </Card>
+  );
 }
+
 export default App;
