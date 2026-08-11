23d2777633da203b791152ebd3e1a8a0c3a00ca4
aimanaziz <aimanaziz2000066@gmail.com>
2026-08-11 11:57:24 +0500

    Add styled button example

--- src/App.jsx
+++ src/App.jsx
@@ -15,53 +15,74 @@
 // }
 // export default App;
 
+//Card example
+// import styled from "styled-components";
+// const Card = styled.div`
+//   width: 300px;
+//   padding: 25px;
+//   margin: 50px auto;
+//   background-color: white;
+//   border-radius: 15px;
+//   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+//   text-align: center;
+// `;
+// const Title = styled.h2`
+//   color: purple;
+//   margin-bottom: 10px;
+// `;
+
+// const Text = styled.p`
+//   color: gray;
+//   font-size: 16px;
+// `;
+
+// const Button = styled.button`
+//   background-color: purple;
+//   color: white;
+//   border: none;
+//   padding: 10px 20px;
+//   border-radius: 8px;
+//   cursor: pointer;
+
+//   &:hover {
+//     background-color: darkviolet;
+//   }
+// `;
+
+// function App() {
+//   return (
+//     <Card>
+//       <Title>Styled Card</Title>
+//       <Text>
+//         This card is created using styled-components.
+//       </Text>
+//       <Button>Learn More</Button>
+//     </Card>
+//   );
+// }
+// export default App;
+
+//Button Example
 import styled from "styled-components";
-
-const Card = styled.div`
-  width: 300px;
-  padding: 25px;
-  margin: 50px auto;
-  background-color: white;
-  border-radius: 15px;
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-  text-align: center;
-`;
-
-const Title = styled.h2`
-  color: purple;
-  margin-bottom: 10px;
-`;
-
-const Text = styled.p`
-  color: gray;
-  font-size: 16px;
-`;
-
 const Button = styled.button`
   background-color: purple;
   color: white;
   border: none;
-  padding: 10px 20px;
+  padding: 12px 25px;
   border-radius: 8px;
+  font-size: 18px;
   cursor: pointer;
-
   &:hover {
-    background-color: darkviolet;
+    background-color: pink;
+    color: black;
   }
 `;
 
 function App() {
   return (
-    <Card>
-      <Title>Styled Card</Title>
-
-      <Text>
-        This card is created using styled-components.
-      </Text>
-
-      <Button>Learn More</Button>
-    </Card>
+    <>
+      <Button>Click Me</Button>
+    </>
   );
 }
-
 export default App;
