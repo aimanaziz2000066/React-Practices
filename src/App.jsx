b49af6c0b2460de4dbfc5ad8ff11726ae2a77a9e
aimanaziz <aimanaziz2000066@gmail.com>
2026-08-11 11:49:50 +0500

    Add styled components example

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  color: purple;
+  text-align: center;
+`;
+
+function App(){
+  return(
+    <>
+    <Heading>Hello Styled Component!</Heading>
+    </>
+  )
+}
+export default App;
